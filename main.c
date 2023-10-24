f714ab1bb13b01af0b6077976c4df3ccec1bd703
Revkxd <79917173+Revkxd@users.noreply.github.com>
2023-10-24 15:14:06 +0800

    C file

---
+++ main.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct {
+    int process_id;
+    int arrival_time;
+    int burst_time;
+} Process;
+
+void FCFS(Process processes[]) {
+
+}
+
+void SJF(Process processes[]) {
+
+}
+
+void SRTF(Process processes[]) {
+
+}
+
+void RR(Process processes[], int quantum) {
+
+}
+
+int main() {
+    int algo, num_processes, quantum;
+    scanf("%d %d %d", &algo, &num_processes, &quantum);
+
+    //value of X is not the Round-Robin algorithm, this value (quantum Z) must be set to 1 but ignored.
+    if(algo != 3) {
+        quantum = 1;
+    }
+    if(num_processes < 3 || num_processes > 100) {
+        printf("Not in scope\n");
+        return 0;
+    }
+
+    // num_processes rows for total number of processes
+    // 3 columns for A B C (process id, arrival_time, burst time)
+    // read the specs if hindi magets nakasulat
+    Process processes[num_processes]; 
+    for(int i = 0; i < num_processes; i++) {
+        scanf("%d %d %d", &processes[i].process_id, &processes[i].arrival_time, &processes[i].burst_time);
+    }
+
+    switch(algo) {
+        case 0: FCFS(processes); break;
+        case 1: SJF(processes); break;
+        case 2: SRTF(processes); break;
+        case 3: RR(processes, quantum);
+    }
+    return 0;
+}
