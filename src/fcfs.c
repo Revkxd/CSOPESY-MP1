e6243ffe3a938b2a9966553500bbf4b35657c85e
zen-young <renzcruz50@gmail.com>
2023-10-25 22:33:41 +0800

    computed average waiting time

--- src/fcfs.c
+++ src/fcfs.c
@@ -1,22 +1,24 @@
 #include "include/scheduling.h"
 
-void FCFS(ProcessQueue pq)
+void FCFS(ProcessQueue *pq)
 {
-    sortArrival(pq);
+    sortArrival(*pq);
 
     // first arrived process has no wait time
-    Process *first = &pq.processes[0];
+    Process *first = &pq->processes[0];
     first->start_time = first->arrival_time;
     first->end_time = first->start_time + first->burst_time;
     first->waiting_time = 0;
 
-    for (int i = 1; i < pq.size; i++) {
-        Process *curr = &pq.processes[i];
-        Process *prev = &pq.processes[i - 1];
+    int total_wait = 0;
+    for (int i = 1; i < pq->size; i++) {
+        Process *curr = &pq->processes[i];
+        Process *prev = &pq->processes[i - 1];
         curr->start_time = prev->end_time;
         curr->end_time = curr->start_time + curr->burst_time;
         curr->waiting_time = curr->end_time - curr->arrival_time - curr->burst_time;
+        total_wait += curr->waiting_time;
     }
 
-    sortPID(pq);
+    pq->average_wait = (float)total_wait / (float)pq->size;
 }
