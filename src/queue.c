4881538d565dadf46214b2b4af799449e9fbdd21
zen-young <renzcruz50@gmail.com>
2023-10-25 22:22:10 +0800

    implemented queue sorting

--- src/queue.c
+++ src/queue.c
@@ -37,3 +37,27 @@ void freeProcessQueue(ProcessQueue pq)
     free(pq.arrival);
     free(pq.burst);
 }
+
+int cmpArrival(const void *a, const void *b)
+{
+    Process *p_a = (Process*)a;
+    Process *p_b = (Process*)b;
+    return p_a->arrival_time - p_b->arrival_time;
+}
+
+int cmpPID(const void *a, const void *b)
+{
+    Process *p_a = (Process*)a;
+    Process *p_b = (Process*)b;
+    return p_a->pid - p_b->pid;
+}
+
+void sortArrival(ProcessQueue pq)
+{
+    qsort(pq.processes, pq.size, sizeof(Process), cmpArrival);
+}
+
+void sortPID(ProcessQueue pq)
+{
+    qsort(pq.processes, pq.size, sizeof(Process), cmpPID);
+}
