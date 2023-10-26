dcc3474a23fb1c1b7bf0a294aaaeff95728252f3
zen-young <renzcruz50@gmail.com>
2023-10-26 23:38:20 +0800

    implemented process queue

--- src/include/queue.h
+++ src/include/queue.h
@@ -1,3 +1,17 @@
 #pragma once
 
 #include "process.h"
+
+typedef struct {
+    Process **queue;
+    int capacity;
+    int front;
+    int rear;
+    int size;
+} ProcessQueue;
+
+ProcessQueue *createQueue(int capacity);
+void freeQueue(ProcessQueue *pq);
+void enqueue(ProcessQueue *pq, Process *p);
+Process* dequeue(ProcessQueue *pq);
+Process* peek(ProcessQueue *pq);
---
+++ src/queue.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "include/queue.h"
+
+ProcessQueue *createQueue(int capacity)
+{
+    ProcessQueue *q = malloc(sizeof(ProcessQueue));
+    q->queue = malloc(sizeof(Process*) * capacity);
+    
+    if (q == NULL || q->queue == NULL)
+    {
+        fprintf(stderr, "Error: Failed to allocate queue\n");
+        return NULL;
+    }
+
+    q->capacity = capacity;
+    q->size = 0;
+    q->rear = -1;
+    q->front = 0;
+    
+    return q;
+}
+
+void freeQueue(ProcessQueue *pq)
+{
+    free(pq->queue);
+    free(pq);
+}
+
+int isFull(ProcessQueue *pq)
+{
+    return (pq->size == pq->capacity);
+}
+
+int isEmpty(ProcessQueue *pq)
+{
+    return (pq->size == 0);
+}
+
+void enqueue(ProcessQueue *pq, Process *p)
+{
+    if (isFull(pq)) {
+        fprintf(stderr, "Error: Queue full\n");
+        return;
+    }
+    pq->rear = (pq->rear + 1) % pq->capacity;
+    pq->queue[pq->rear] = p;
+    pq->size++;
+}
+
+Process* dequeue(ProcessQueue *pq)
+{
+    if(isEmpty(pq)) {
+        fprintf(stderr, "Error: Queue empty\n");
+        return NULL;
+    }
+    Process *p = pq->queue[pq->front];
+    pq->front = (pq->front + 1) % pq->capacity;
+    pq->size--;
+
+    return p;
+}
+
+Process* peek(ProcessQueue *pq)
+{
+    if (isEmpty(pq)) return NULL;
+    return pq->queue[pq->front];
+}
