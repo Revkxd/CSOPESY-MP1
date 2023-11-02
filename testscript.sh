fe902e2217bbe2a4e9ca65ab4151c686417eed60
zen-young <renzcruz50@gmail.com>
2023-11-02 17:48:58 +0800

    modified testscript

--- testscript.sh
+++ testscript.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+output_file="./__test_output__"
+
 if [ $# -ne 2 ]; then
     echo "Usage: $0 <input_file> <output_file_to_compare_to>"
     exit 0
 fi
 
-build/a.out < $1 | tee >(diff $2 -)
+build/a.out < $1 | tee $output_file
+echo -e "\n\n===== Diff ====="
+diff -y --suppress-common-lines $output_file $2
+rm $output_file
+echo -e "\n===== End of Diff ====="
