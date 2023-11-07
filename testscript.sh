21a9790b13a778469232f34d8c7b173c8da834ac
zen-young <renzcruz50@gmail.com>
2023-11-07 10:17:02 +0800

    added bulk testing to test script

--- testscript.sh
+++ testscript.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
-output_file="./__test_output__"
+bin="build/a.out"
+test_dir="test"
+diff_output="./__DIFF__"
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <input_file> <output_file_to_compare_to>"
+txtred=$(tput setaf 1) # Red
+txtgrn=$(tput setaf 2) # Green
+txtwht=$(tput setaf 7) # White
+
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+    echo -e "Usage:\n$0 <input_file> <output_file_to_compare_to>\n\tOR\n$0 <algorithm>"
     exit 0
 fi
 
-build/a.out < $1 | tee $output_file
-echo -e "\n\n===== Diff ====="
-diff -y --suppress-common-lines $output_file $2
-rm $output_file
-echo -e "\n===== End of Diff ====="
+if [ $# -eq 1 ]; then
+    inputs=($test_dir/$1/input??.txt)
+    outputs=($test_dir/$1/output??.txt)
+    for ((i=0; i<${#inputs[@]}; i++)); do
+        $bin < ${inputs[$i]} > $diff_output
+        diff $diff_output ${outputs[$i]} &> /dev/null
+        if [ $? -eq 0 ]; then
+            echo "${txtgrn}[PASSED] ${txtwht}${inputs[$i]}"
+        else
+            echo "${txtred}[FAILED] ${txtwht}${inputs[$i]}"
+        fi
+    done
+fi
+
+if [ $# -eq 2 ]; then
+    $bin < $1 | tee $output_file
+    echo -e "\n\n===== Diff ====="
+    diff -y --suppress-common-lines $diff_output $2
+    echo -e "\n===== End of Diff ====="
+fi
+
+rm $diff_output
