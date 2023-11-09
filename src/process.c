3d71b539e4ca0cbb3397131c4b66f5d4c7e11fe8
zen-young <renzcruz50@gmail.com>
2023-11-09 22:55:58 +0800

    changed printing code

--- src/process.c
+++ src/process.c
@@ -52,11 +52,8 @@ void printProcessList(ProcessList *pl)
 {
     for (int i = 0; i < pl->size; i++) {
         Process *p = pl->processes[i];
-        printf("%d start time: %d end time: %d | Waiting time: %d\n", 
-            p->pid,
-            p->start_time,
-            p->end_time,
-            p->waiting_time
-        );
+        for (int j = 0; p->start_time[j] != -1; j++)
+            printf("%d start time: %d end time: %d | ", p->start_time[j], p->end_time[j]);
+        printf("Waiting time: %d\n", p->waiting_time);
     }
 }
