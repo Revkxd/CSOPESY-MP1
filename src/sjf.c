8327bb6cc9918aea80f8cb3a1476db770fe70b20
zen-young <renzcruz50@gmail.com>
2023-11-12 23:54:33 +0800

    optimization: improved managing of ready queue using heap

--- src/sjf.c
+++ src/sjf.c
@@ -3,15 +3,16 @@
 #include "include/scheduling.h"
 #include "include/queue.h"
 #include "include/sort.h"
+#include "include/heap.h"
 
 void SJF(ProcessList *pl)
 {
     int time = 0;
     int total_wait = 0;
     ProcessQueue *arrival_queue = createQueue(pl->size);
-    Process **ready_table = calloc(pl->size, sizeof(Process*));
+    ProcessHeap *ready_queue = createHeap(pl->size, BURST);
 
-    if (arrival_queue == NULL || ready_table == NULL) {
+    if (arrival_queue == NULL || ready_queue == NULL) {
         fprintf(stderr, "%s", "Error: Unable to allocate\n");
         return;
     }
@@ -22,25 +23,20 @@ void SJF(ProcessList *pl)
 
     sortQueueArrival(arrival_queue);
 
-    int active_processes = 0;
-    while (arrival_queue->size > 0 || active_processes > 0) {
+    while (arrival_queue->size > 0 || ready_queue->size > 0) {
         while (peek(arrival_queue) != NULL && peek(arrival_queue)->arrival_time <= time) {
-            insertProcess(ready_table, pl->size, active_processes, dequeue(arrival_queue));
-            active_processes++;
+            insertHeap(ready_queue, dequeue(arrival_queue));
         }
 
-        if (active_processes == 0) {
+        if (ready_queue->size == 0) {
             time = peek(arrival_queue)->arrival_time;
             continue;
         }
 
-        int minIdx = findMinBurst(ready_table, active_processes);
-        Process* running = ready_table[minIdx];
+        Process* running = extractMin(ready_queue);
         appendStartTime(running, time);
         appendEndTime(running, time + running->burst_time);
-        removeProcess(ready_table, pl->size, minIdx);
         running->remaining_burst = 0;
-        active_processes--;
         // compute wait time of completed process
         running->waiting_time = time - running->arrival_time;
         total_wait += running->waiting_time;
@@ -49,6 +45,6 @@ void SJF(ProcessList *pl)
 
     pl->ave_wait_time = (float)total_wait / (float)pl->size;
 
-    free(ready_table);
+    freeHeap(ready_queue);
     freeQueue(arrival_queue);
 }
