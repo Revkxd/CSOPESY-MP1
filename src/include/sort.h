66172ff57d28e62b37ea1f698492fd07542d7753
zen-young <renzcruz50@gmail.com>
2023-11-13 01:18:42 +0800

    deprecated sort.h

--- src/include/sort.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "process.h"
-#include "queue.h"
-
-void sortArrival(Process **pl, int size);
-void sortBurst(Process **pl, int size);
-void sortPID(Process **pl, int size);
-void sortQueueArrival(ProcessQueue* pq);
